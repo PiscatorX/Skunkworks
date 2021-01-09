300569c4de35319fbacec35f52abc53ccb623928
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-09 21:30:05 +0200

    get counts

---
+++ RNA-seq-workshop/RNA-analysisx.R
@@ -0,0 +1,44 @@
+library(GenomicAlignments)
+library(BiocParallel)
+library(GenomicFeatures)
+library(Rsamtools)
+library(airway)
+library(dplyr)
+
+
+register(SerialParam())
+
+airway_dir  <- system.file("extdata", package = "airway", mustWork = T)
+
+list.files(airway_dir)
+
+csvfile <-  file.path(airway_dir, "sample_table.csv")
+
+(sample_table <-  read.csv(csvfile, row.names = 1))
+
+nrow(sample_table)
+
+sample_table %>% group_by(dex) %>% summarise(Count= n())
+
+(filenames <- file.path(airway_dir, paste0(sample_table$Run,"_subset.bam")))
+
+file.exists(filenames)
+
+(bam_files <- BamFileList(filenames,  yieldSize = 2000000))
+
+seqinfo(bam_files[1])
+
+gtf <- file.path(airway_dir,  "Homo_sapiens.GRCh37.75_subset.gtf")
+(gtf_txdb <- makeTxDbFromGFF(gtf, format = "gtf",  circ_seqs = character()))
+
+(ebg <- exonsBy(gtf_txdb, by="gene"))
+
+
+se <- summarizeOverlaps(features = ebg,
+                        reads = bam_files,
+                        mode = "Union",
+                        singleEnd = F,
+                        ignore.strand = T,
+                        fragments = T)
+                        
+
