ba4d0a179234ee40767ea2a8a74e3535426f279c
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-09-03 11:03:54 +0200

    update app

--- zeolite_app/app.R
+++ zeolite_app/app.R
@@ -1,102 +1,169 @@
 library(shiny)
 
+
 ui <- fluidPage(
-
-  selectInput("Metals", 
-              "Metals:", 
-              choices = c('Na+', 'Ag+', 'Cu+', 'Ce+4', 'Cs+2', 'Ni+2'),
-              multiple = TRUE),
-  
-  sliderInput("Sa",
-              "Sa",
-              141.4,
-              720.0,
-              492.55379213483155),
-  
-  sliderInput("Vmicro",
-              "Vmicro",
-              0.08,
-              0.6659999999999999,
-              0.2569763636363636),
-  
-  sliderInput("Vmeso",
-              "Vmeso",
-              0.04,
-              0.18,
-              0.0808421052631579),
-  
-  sliderInput("Pore_Size",
-              "Pore_Size",
-              0.83,
-              3.1729,
-              1.3090391509433963),
-  
-  sliderInput("Si_Al",
-              "Si_Al",
-              1.031,
-              46.0,
-              7.295000006952248),
-  
-  sliderInput("Na+",
-              "Na+",
-              0.02,
-              1.0425383540000002,
-              0.49715446279710146),
-  
-  sliderInput("Ag+",
-              "Ag+",
-              0.297,
-              1.18845,
-              0.8477998749909909),
-  
-  sliderInput("Cu+",
-              "Cu+",
-              0.16899999999999998,
-              0.467,
-              0.2289083723975904),
-  
-  sliderInput("Ce+4",
-              "Ce+4",
-              0.21,
-              2.29489,
-              0.9387571653543307),
-  
-  sliderInput("Cs+2",
-              "Cs+2",
-              0.666666667,
-              0.666666667,
-              0.666666667),
-  
-  sliderInput("Ni+2",
-              "Ni+2",
-              0.024232082000000002,
-              0.3125,
-              0.10820202342857144),
-  
-  sliderInput("Dipole_Moment",
-              "Dipole_Moment",
-              0.51,
-              0.79,
-              0.6308426966292134),
-  
-  sliderInput("Chemical_Hardness",
-              "Chemical_Hardness",
-              3.0401,
-              5.602,
-              4.074383707865168),
-  
-  sliderInput("Kinetic_Diameter ",
-              "Kinetic_Diameter ",
-              0.77,
-              0.91,
-              0.8232584269662919),
-  
+ 
   
 
+  selectInput("Adsorbent", 
+              "Adsorbent:", 
+              choices = c('CuAgY', 'CuCeY', 'NiCeY', 'AgY', 'clinoptilolite', 'CeY', 'CuY', 'CuX', 'NiY', 'CsY', 'NaY', 'MCM-22', 'AgCeY', 'AgX', 'CuHY'),
+              multiple = FALSE),
+  
+  selectInput("Adsorbate", 
+              "Adsorbate:", 
+              choices = c('TP', 'BT', 'DBT'),
+              multiple = FALSE),
+  
+  selectInput("Solvent", 
+              "Solvent:", 
+              choices = c('cyclohexane', '1-octane', 'n-octane', 'n-heptane', 'iso-octane', 'n-Heptane', 'n-Octane', 'ether', 'hexadecane'),
+              multiple = FALSE),
+  
+  numericInput("Sa",
+               "Sa",
+               min = 141.4,
+               max = 720.0,
+               value = 0),
+  
+  numericInput("Vmicro",
+               "Vmicro",
+               min = 0.08,
+               max = 0.666,
+               value = 0),
+  
+  # numericInput("Vmeso",
+  #              "Vmeso",
+  #              min = 0.04,
+  #              max = 0.18,
+  #              value = 0),
+  # 
+  # numericInput("Pore_Size",
+  #              "Pore_Size",
+  #              min = 0.83,
+  #              max = 3.1729,
+  #              value = 0),
+  # 
+  # numericInput("Si_Al",
+  #              "Si_Al",
+  #              min = 1.031,
+  #              max = 46.0,
+  #              value = 0),
+  # 
+  # numericInput("Na+",
+  #              "Na+",
+  #              min = 0.02,
+  #              max = 1.042538354,
+  #              value = 0),
+  # 
+  # numericInput("Ag+",
+  #              "Ag+",
+  #              min = 0.297,
+  #              max = 1.18845,
+  #              value = 0),
+  # 
+  # numericInput("Cu+",
+  #              "Cu+",
+  #              min = 0.169,
+  #              max = 0.467,
+  #              value = 0),
+  # 
+  # numericInput("Ce+4",
+  #              "Ce+4",
+  #              min = 0.21,
+  #              max = 2.29489,
+  #              value = 0),
+  # 
+  # numericInput("Cs+2",
+  #              "Cs+2",
+  #              min = 0.666666667,
+  #              max = 0.666666667,
+  #              value = 0),
+  # 
+  # numericInput("Ni+2",
+  #              "Ni+2",
+  #              min = 0.024232082,
+  #              max = 0.3125,
+  #              value = 0),
+  # 
+  # numericInput("Dipole_Moment",
+  #              "Dipole_Moment",
+  #              min = 0.51,
+  #              max = 0.79,
+  #              value = 0),
+  # 
+  # numericInput("Chemical_Hardness",
+  #              "Chemical_Hardness",
+  #              min = 3.0401,
+  #              max = 5.602,
+  #              value = 0),
+  # 
+  # numericInput("Kinetic_Diameter ",
+  #              "Kinetic_Diameter ",
+  #              min = 0.77,
+  #              max = 0.91,
+  #              value = 0),
+  
+  tableOutput("tablex")
+  
 )
 
-server <- function(input, output, session) {
+
   
+  
+               #      get_zeo <- reactive(data.frame(SA=input$SA,
+               #                          Vmicro=input$Vmicro,
+               #                          Vmeso=input$Vmeso,
+               #                          pore_size=input$pore_size,
+               #                          Si_Al=input$Si_Al,
+               #                          `Na+`=input$`Na+`,
+               #                          `Ag+`=input$`Ag+`,
+               #                          `Cu+`=input$`Cu+`,
+               #                          `Ce+4`=input$`Ce+4`,
+               #                          `Cs+2`=input$`Cs+2`,
+               #                          `Ni+2`=input$`Ni+2`,
+               #                          `x_Na+`=input$`x_Na+`,
+               #                          `x_Ag+`=input$`x_Ag+`,
+               #                          `x_Cu+`=input$`x_Cu+`,
+               #                          `x_Ce+4`=input$`x_Ce+4`,
+               #                          `x_Cs+2`=input$`x_Cs+2`,
+               #                          `x_Ni+2`=input$`x_Ni+2`,
+               #                          `R_Na+`=input$`R_Na+`,
+               #                          `R_Ag+`=input$`R_Ag+`,
+               #                          `R_Cu+`=input$`R_Cu+`,
+               #                          `R_Ce+4`=input$`R_Ce+4`,
+               #                          `R_Cs+2`=input$`R_Cs+2`,
+               #                          `R_Ni+2`=input$`R_Ni+2`,
+               #                          adsorbate=input$adsorbate,
+               #                          dipole_moment=input$dipole_moment,
+               #                          chemical_hardness=input$chemical_hardness,
+               #                          kinetic_diameter=input$kinetic_diameter,
+               #                          C_0=input$C_0,
+               #                          solvent=input$solvent,
+               #                          oil_adsorbent_ratio=input$oil_adsorbent_ratio,
+               #                          Temp=input$Temp,
+               #                          Capacity=input$Capacity))
+               # 
+               # metal_props <- reactive(c('x_Na+'=26.2,
+               #                           'x_Ag+'=14.6, 
+               #                           'x_Cu+'=14.0, 
+               #                           'x_Ce+4'=3.038, 
+               #                           'x_Cs+2'=0.666666667, 
+               #                           'x_Ni+2'=27.2, 
+               #                           'R_Na+'=1.02, 
+               #                           'R_Ag+'=1.15, 
+               #                           'R_Cu+'=0.77, 
+               #                           'R_Ce+4'=0.87,
+               #                           'R_Cs+2'=1.67, 
+               #                           'R_Ni+2'=0.7))  
+server <- function(input, output) {
+
+
+  
+
 }
 
 shinyApp(ui, server)
+
+
+#     
--- zeolite_app/buildshiny.py
+++ zeolite_app/buildshiny.py
@@ -13,26 +13,26 @@ class Table2Buttons(GetZeoliteTsv):
         self.metals = ['Na+','Ag+', 'Cu+', 'Ce+4', 'Cs+2', 'Ni+2']
         self.metal_vars = [ col  for col in self.zeolite_df.columns if col.startswith("x_") or col.startswith("R_")]
         self.metal_props = dict([ (var, self.zeolite_df[var].max()) for var in self.metal_vars ])
-        self.selectInput = [ col for col,dtype  in dict(self.zeolite_df.dtypes).items() if dtype == 'O' ]
+        self.selectInput = [ col for col, dtype  in dict(self.zeolite_df.dtypes).items() if isinstance(dtype, pd.core.dtypes.dtypes.CategoricalDtype)  ]
         self.sliderInput =  [ col for col,dtype  in dict(self.zeolite_df.dtypes).items() if dtype == 'float64' ]
+
         
+
         
-       
     def get_Input(self):
 
         for input_col1 in self.selectInput:
             choices = self.zeolite_df[input_col1].unique()
             self.tag_selectInput(input_col1, tuple(choices))
+            
 
-        self.tag_selectInput("Metals", tuple(self.metals), multiple ='TRUE')    
+        #self.tag_selectInput("Metals", tuple(self.metals), multiple ='TRUE')    
             
         for input_col2 in self.sliderInput:
             metrics = self.zeolite_df[input_col2].describe()
             if input_col2 in  self.metal_vars:continue
             if input_col2 == 'Capacity':continue
-            
             self.tag_sliderInput(input_col2, metrics['min'], metrics['max'], metrics['mean'])
-
     
     def tag_selectInput(self, inputId, choices, multiple ='FALSE'):
         
@@ -46,29 +46,25 @@ multiple = {multiple}),\n""".format(**{"inputId": inputId.title(), "choices": re
 
     def tag_sliderInput(self, inputId, vmin, vmax, mean):
 
-        print("""sliderInput("{inputId}",
+        print("""numericInput("{inputId}",
 "{inputId}",
-{min},
-{max},
-{mean}),\n""".format(**{"inputId": inputId.title(),"min": vmin,"max": vmax, "mean": mean})) 
-
-
-
+min = {min},
+max = {max},
+value = 0),\n""".format(**{"inputId": inputId.title(),"min": vmin,"max": vmax, "mean": mean})) 
 
         
+    def print_vars(self):
+
+        print(', '.join(['$'.join(['input',col]) for col in self.zeolite_df.columns ]))
+         
         
 
-        
+         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="build shinu app buttons")
     parser.add_argument('zeolite_file',help = "zeolite file")
     args = parser.parse_args()
     t2b = Table2Buttons(args.zeolite_file)
-    t2b.set_dtypes()
     t2b.get_Input()
-    #for metal in ['Na+','Ag+', 'Cu+', 'Ce+4', 'Cs+2', 'Ni+2', 'x_Na+', 'x_Ag+', 'x_Cu+','x_Ce+4', 'x_Cs+2', 'x_Ni+2', 'R_Na+', 'R_Ag+', 'R_Cu+', 'R_Ce+4','R_Cs+2', 'R_Ni+2']:
-    #     print(meta)
-   
-    #t2b.get_selectInput()
-    #t2b.get_sliderInput()
+    t2b.print_vars()
     
