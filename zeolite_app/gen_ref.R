68a98a7b58e52117b0a130b9568b906a59da991f
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-09-06 15:13:27 +0200

    generate reference

---
+++ zeolite_app/gen_ref.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(plotly)
+library(caret)
+library(stats)
+library(dplyr)
+
+
+
+zeo_ref <- read.table("zeolites_DB.tsv", header = T, sep = "\t", stringsAsFactors = F ) 
+
+numeric_cols  <- zeo_ref %>%
+                 select_if(is.numeric) %>%
+                 colnames()
+
+for (col in numeric_cols){
+  zeo_ref[col] <- 0
+  
+}
+
+zeo_ref_uniq <- do.call(cbind,   lapply(zeo_ref,   unique)) %>% 
+                data.frame()
+
+write.table(zeo_ref_uniq, "zeolites_DBX.tsv", sep = "\t", quote = F, row.names = FALSE)
+
